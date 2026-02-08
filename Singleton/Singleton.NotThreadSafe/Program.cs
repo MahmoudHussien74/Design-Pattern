b36dd17471f2524dc110723bb9831d3cd08039a9
Mahmoud Hussien <mahmoudhussien.m74>
2026-02-08 02:44:53 +0200

    Singleton Design Pattern

---
+++ Singleton/Singleton.NotThreadSafe/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Singleton.NotThreadSafe
+{
+    internal class Program
+    {
+        static MemoryLogger logger;
+
+        static void Main(string[] args)
+        {
+            AssignVoucher("mahmoud@google.com", "MHS2026");
+
+            UseVoucher("MHS2026");
+
+            logger.ShowLog();
+
+            Console.ReadKey();
+        }
+
+        static void AssignVoucher(string email, string voucher)
+        {
+            logger = MemoryLogger.GetLogger;
+            // Logic here
+            logger.LogInfo($"Voucher '{voucher}' assigned");
+
+            // another logic
+            logger.LogError($"unable to send email '{email}'");
+        }
+        static void UseVoucher(string voucher)
+        {
+            logger = MemoryLogger.GetLogger;
+            // Logic here
+            logger.LogWarning($"3 attempts made to validate the voucher");
+
+            // Logic here
+            logger.LogInfo($"'{voucher}' is used");
+        }
+    }
+}
